dad8dcb065166d4fdc1baf3ab3bc6f8db30205aa
Krishna Kumar M <65696644+KrishnaKumar2002@users.noreply.github.com>
2022-01-30 23:22:26 +0530

    Add files via upload

--- Wage slip.cpp
+++ Wage slip.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<stdlib.h>
+#include<stdio.h>
+#define n 10
 using namespace std;
 class sales{
     int sno;
@@ -9,33 +11,39 @@ class sales{
         totalwage=whr*wage;
     }
     public:
-        void in_data();
-        void out_data();
-};
-void sales::in_data(){
+        void in_data(int);
+        void out_data(int);
+}sal[10];
+void sales::in_data(int x){
     wage=100.28;
     totalwage=0.0;
     cout<<"\nEnter the id of employee";
-    cin>>sno;
+    cin>>sal[x].sno;
     cout<<"Enter name of employee";
-    gets(sname);
+    gets(sal[x].sname);
     cout<<"\nEnter the total working hours";
-    cin>>whr;
+    cin>>sal[x].whr;
     
     calcwage();
 }
-void sales::out_data(){
+void sales::out_data(int y){
     cout<<"\n\t\t\t\t\t\t\t KK & co ";
     cout<<"\n\t\t\t\t\t\t\t ~~~~~~~"<<"\n";
-    cout<<"\n\t\t\t\t\t\t\t ID:"<<sno<<"\n";
-    cout<<"\n\t\t\t\t\t\t\t Name:"<<sname<<"\n";
-    cout<<"\n\t\t\t\t\t\t\t wage per hour:"<<"$"<<wage<<"\n";
-    cout<<"\n\t\t\t\t\t\t\t Total hours worked:"<<whr<<"\n";
-    cout<<"\n\t\t\t\t\t\t\t Total Wages:"<<"$"<<totalwage<<"\n";
+    cout<<"\n\t\t\t\t\t\t\t ID:"<<sal[y].sno<<"\n";
+    cout<<"\n\t\t\t\t\t\t\t Name:"<<sal[y].sname<<"\n";
+    cout<<"\n\t\t\t\t\t\t\t wage per hour:"<<"$"<<sal[y].wage<<"\n";
+    cout<<"\n\t\t\t\t\t\t\t Total hours worked:"<<sal[y].whr<<"\n";
+    cout<<"\n\t\t\t\t\t\t\t Total Wages:"<<"$"<<sal[y].totalwage<<"\n";
 }
+    //sales sal[10];
+
 int main(){
-    sales sal;
-    sal.in_data();
-    sal.out_data();
+
+    for(int i=0;i<n;i++){
+        sal[i].in_data(i);
+    }
+    for(int i=0;i<n;i++){
+            sal[i].out_data(i);
+    }
     return 0;
 }
